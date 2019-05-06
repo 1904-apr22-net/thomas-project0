af63c031f5b5658300a22061621b248126f9cf98
Thomas Llewellyn <llewellyn.speck@gmail.com>
2019-05-06 16:42:56 -0500

    more user interface progress

--- Project_0/Project_0/Program.cs
+++ Project_0/Project_0/Program.cs
@@ -20,9 +20,6 @@ namespace CigarShop.ConsoleUI
         private static readonly ILogger _interfaceLogger = LogManager.GetCurrentClassLogger();
         public static void Main(string[] args)
         {
-            //ICigarShop cigarShop = CreateCigarShop();
-            //XmlSerializer serializer = CreateXmlSerializer();
-
             using (Project0Context dbContext = CreateDbContext())
             {
                 UI();
@@ -52,7 +49,8 @@ namespace CigarShop.ConsoleUI
                     {
                         Console.WriteLine("i: Lookup Cigar by Id Number");
                         Console.WriteLine("n: Lookup Cigar by Name");
-                        Console.WriteLine("m: Lookup Cigar by Manufacturer Name");
+                        Console.WriteLine("d: Lookup Cigar by Manufacturer's Id number");
+                        Console.WriteLine("m: Lookup Cigar by Manufacturer's Name");
                         Console.WriteLine("b: Go Back");
                         Console.WriteLine();
                         Console.WriteLine("Please enter one of the specified menu options. ");
@@ -60,30 +58,115 @@ namespace CigarShop.ConsoleUI
 
                         if (userInput == "i")
                         {
-                            Console.WriteLine("Please enter the Id Number");
+                            Console.WriteLine("Please enter the Cigar's Id Number");
                             userInput = Console.ReadLine();
                         }
                         else if (userInput == "n")
                         {
-
+                            Console.WriteLine("Please enter the Cigar's Name");
+                            userInput = Console.ReadLine();
+                        }
+                        else if (userInput == "d")
+                        {
+                            Console.WriteLine("Please enter the Manufactuer's Id Number");
+                            userInput = Console.ReadLine();
                         }
                         else if (userInput =="m")
                         {
-
+                            Console.WriteLine("Please enter the Manufactuer's Name");
+                            userInput = Console.ReadLine();
                         }
                         else if(userInput == "b")
                         {
-
+                            Console.WriteLine();
+                            break;
                         }
                     }
                 }
                 else if (userInput == "e")
                 {
+                    Console.WriteLine("o: Lookup Order by Order Id");
+                    Console.WriteLine("s: Lookup Order by Store Id");
+                    Console.WriteLine("c: Lookup Order by Customer Last Name");
+                    Console.WriteLine("b: Go Back");
+                    Console.WriteLine();
+                    Console.WriteLine("Please enter one of the specified menu options. ");
+                    userInput = Console.ReadLine();
 
+                    while (true)
+                    {
+                        if (userInput == "o")
+                        {
+                            Console.WriteLine("Please enter the Order Id Number. ");
+                            userInput = Console.ReadLine();
+                        }
+                        else if (userInput == "s")
+                        {
+                            Console.WriteLine("Please enter the Store's Id Number. ");
+                            userInput = Console.ReadLine();
+                        }
+                        else if (userInput == "c")
+                        {
+                            Console.WriteLine("Please enter the Customer's Last Name. ");
+                            userInput = Console.ReadLine();
+                        }
+                        else if (userInput == "b")
+                        {
+                            break;
+                        }
+                    }
                 }
                 else if (userInput == "s")
                 {
+                    Console.WriteLine("v: View Store Locations");
+                    Console.WriteLine("a: Add a New Store");
+                    Console.WriteLine("r: Remove a Store");
+                    Console.WriteLine("b: Go Back");
+                    Console.WriteLine();
+                    Console.WriteLine("Please enter one of the specified menu options. ");
+                    userInput = Console.ReadLine();
 
+                    while (true)
+                    {
+                        if (userInput == "v")
+                        {
+                            Console.WriteLine("i: Lookup store by Store Id");
+                            Console.WriteLine("l: Lookup store by Location");
+                            Console.WriteLine("b: Go Back");
+                            Console.WriteLine();
+                            Console.WriteLine("Please enter one of the specified menu options. ");
+                            userInput = Console.ReadLine();
+
+                            while (true)
+                            {
+                                if (userInput == "i")
+                                {
+                                    Console.WriteLine("Please enter the store's Id Number");
+                                    userInput =Console.ReadLine();
+                                }
+                                else if (userInput =="l")
+                                {
+
+                                }
+                                else if (userInput == "b")
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        if (userInput == "a")
+                        {
+
+                        }
+                        if (userInput == "r")
+                        {
+
+                        }
+                        if (userInput == "b")
+                        {
+                            break;
+                        }
+                    }
                 }
                 else if (userInput == "i")
                 {
@@ -105,7 +188,7 @@ namespace CigarShop.ConsoleUI
             var optionsBuilder = new DbContextOptionsBuilder<Project0Context>();
             optionsBuilder
                 .UseSqlServer(SecretConfiguration.ConnectionString);
-                //.UseLoggerFactory(AppLoggerFactory);
+
 
             return new Project0Context(optionsBuilder.Options);
         }
